a5f21dcee5f21a7ba4d0d297f7a814b48c1c0547
riyaz <riyazuddin515@gmail.com>
2022-09-16 14:37:34 +0530

    typing indicator implemented

--- src/components/chat/ChatBox.js
+++ src/components/chat/ChatBox.js
@@ -1,15 +1,18 @@
 import { ArrowBackIcon, InfoIcon } from '@chakra-ui/icons'
-import { Box, Heading, IconButton, Input, useToast, Text } from '@chakra-ui/react'
+import { Box, Heading, IconButton, Input, useToast, Text, Spinner } from '@chakra-ui/react'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { ChatState } from '../../context/ChatContext'
-import { getOtherUser, isLastMessage, isSameUser, isSenderLastMessage } from '../../utils/UtilityFunctions'
+import { getOtherUser, isSenderLastMessage } from '../../utils/UtilityFunctions'
 import SingleMessage from '../SingleMessage'
 import { io } from 'socket.io-client'
 import ProfileModal from '../ProfileModal'
 import GroupInfoModal from '../group_chat/GroupInfoModal'
-// import './ChatBox.css'
+import { useRef } from 'react'
+import typingGIF from '../../asset/loading.gif'
 let socket = io('http://localhost:4000');
+let previousTimeout;
+let selectedChatCopy;
 
 const ChatBox = () => {
 
@@ -17,6 +20,9 @@ const ChatBox = () => {
     const [newMessage, setNewMessage] = useState('')
     const [messages, setMessages] = useState([])
     const [newArrivalMessage, setNewArrivalMessage] = useState(null)
+    const [typing, setTyping] = useState(false)
+    const lastEmptyElement = useRef()
+    const [loading, setLoading] = useState(false)
 
     const toast = useToast()
 
@@ -25,12 +31,34 @@ const ChatBox = () => {
             setNewArrivalMessage(message)
             updateLastMessage(message)
         })
+        socket.on('typing', (chatId) => {
+            if (selectedChatCopy && selectedChatCopy?._id === chatId) {
+                setTyping(true)
+            }
+        })
+        socket.on('stop-typing', (chatId) => {
+            if (selectedChatCopy && selectedChatCopy?._id === chatId) {
+                setTyping(false)
+            }
+        })
     })
 
     useEffect(() => {
+        selectedChatCopy = selectedChat
+        setNewMessage('')
+        setMessages([])
+        setNewArrivalMessage(null)
+        setTyping(false)
+    }, [selectedChat])
 
+
+    useEffect(() => {
+        lastEmptyElement.current?.scrollIntoView();
+    }, [typing, messages])
+
+    useEffect(() => {
         if (newArrivalMessage) {
-
+            setTyping(false)
             if (chat.length === 0) {
                 setChat([newArrivalMessage.chat]);
                 return;
@@ -55,6 +83,7 @@ const ChatBox = () => {
     useEffect(() => {
         if (selectedChat) {
             const fetchMessages = async () => {
+                setLoading(true)
                 try {
                     const config = {
                         headers: {
@@ -75,6 +104,7 @@ const ChatBox = () => {
                         position: 'bottom-left'
                     })
                 }
+                setLoading(false)
             }
             fetchMessages()
         }
@@ -120,6 +150,20 @@ const ChatBox = () => {
         setNewMessage('')
     }
 
+    const typingHandler = (msg) => {
+        setNewMessage(msg)
+        if (!socket) {
+            return
+        }
+        socket.emit('typing', selectedChat, loggedInUser._id)
+        if (previousTimeout) {
+            clearTimeout(previousTimeout)
+        }
+        previousTimeout = setTimeout(() => {
+            socket.emit('stop-typing', selectedChat, loggedInUser._id)
+        }, 3000)
+    }
+
 
     return (
         <Box
@@ -132,54 +176,58 @@ const ChatBox = () => {
             {
                 selectedChat
                     ?
-                    <Box height='100%'
-                        display='flex' flexDir='column'
-                    >
-                        <Box
-                            p={2}
-                            display='flex' justifyContent='space-between'
+                    loading
+                        ? <Spinner color='green' size='xl' margin='auto' />
+                        : <Box height='100%'
+                            display='flex' flexDir='column'
                         >
-                            <IconButton icon={<ArrowBackIcon />}
-                                onClick={() => setSelectedChat(null)}
-                            />
-                            <Heading size='lg'>{selectedChat.isGroupChat ? selectedChat.chatName : getOtherUser(loggedInUser, selectedChat.users).name}</Heading>
-                            {
-                                selectedChat.isGroupChat
-                                    ? <GroupInfoModal groupChat={selectedChat}>
-                                        <IconButton icon={<InfoIcon />} />
-                                    </GroupInfoModal>
-                                    : <ProfileModal user={getOtherUser(loggedInUser, selectedChat.users)}>
-                                        <IconButton icon={<InfoIcon />} />
-                                    </ProfileModal>
-                            }
-
-                        </Box>
-                        <Box
-                            className='chat-box'
-                            h="100%" p={3} mb={2}
-                            display='flex' flexDir='column-reverse'
-                            borderRadius="lg" overflowY="scroll"
-                            backgroundColor='#E2E2E2'
-                        >
-                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <Box
+                                p={2}
+                                display='flex' justifyContent='space-between'
+                            >
+                                <IconButton icon={<ArrowBackIcon />}
+                                    onClick={() => setSelectedChat(null)}
+                                />
+                                <Heading size='lg'>{selectedChat.isGroupChat ? selectedChat.chatName : getOtherUser(loggedInUser, selectedChat.users).name}</Heading>
                                 {
-                                    messages.length > 0 &&
-                                    messages.map((message, index) => (
-                                        <SingleMessage key={message._id} message={message} loggedInUser={loggedInUser}
-                                            isSenderLastMessage={isSenderLastMessage(messages, index, loggedInUser)}
-                                        />
-                                    ))
+                                    selectedChat.isGroupChat
+                                        ? <GroupInfoModal groupChat={selectedChat}>
+                                            <IconButton icon={<InfoIcon />} />
+                                        </GroupInfoModal>
+                                        : <ProfileModal user={getOtherUser(loggedInUser, selectedChat.users)}>
+                                            <IconButton icon={<InfoIcon />} />
+                                        </ProfileModal>
                                 }
-                            </div>
+
+                            </Box>
+                            <Box
+                                className='chat-box'
+                                h="100%" p={3} mb={2}
+                                display='flex' flexDir='column-reverse'
+                                borderRadius="lg" overflowY="scroll"
+                                backgroundColor='#E2E2E2'
+                            >
+                                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                    {
+                                        messages.length > 0 &&
+                                        messages.map((message, index) => (
+                                            <SingleMessage key={message._id} message={message} loggedInUser={loggedInUser}
+                                                isSenderLastMessage={isSenderLastMessage(messages, index, loggedInUser)}
+                                            />
+                                        ))
+                                    }
+                                    {typing && <img src={typingGIF} alt='loading' style={{ width: '80px', height: '60px', marginBottom: '-15px' }}></img>}
+                                    <span ref={lastEmptyElement}></span>
+                                </div>
+                            </Box>
+                            <form
+                                onSubmit={(e) => sendMessage(e)}
+                            >
+                                <Input
+                                    bg="#E8E8E8" placeholder='Type message here'
+                                    type='text' value={newMessage} onChange={(e) => typingHandler(e.target.value)} />
+                            </form>
                         </Box>
-                        <form
-                            onSubmit={(e) => sendMessage(e)}
-                        >
-                            <Input
-                                bg="#E8E8E8" placeholder='Type message here'
-                                type='text' value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
-                        </form>
-                    </Box>
                     : <Text fontSize='3xl' margin='auto'>Select Chat</Text>
             }
         </Box>
